d15c314be405c16ef86ec34ca652f5d0f39fc49b
Pedro Leao <pedroforestileao@gmail.com>
2021-01-20 19:44:12 -0300

    set global style

---
+++ src/styles/global.ts
@@ -0,0 +1,31 @@
+import {createGlobalStyle} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #312e38;
+    color: #fff;
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, button {
+    font-family: 'Roboto slab', serif;
+    font-size: 16px;
+  }
+
+  h1, h2, h3, h4, h5 ,h6, strong {
+    font-weight: 500;
+  }
+
+  button {
+    cursor: pointer;
+  }
+`;
+
+export default GlobalStyle;
